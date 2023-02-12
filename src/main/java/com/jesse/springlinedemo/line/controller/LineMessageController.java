4b7e2a3659040669c08ba68ae0c1a2901ce2b946
luoyijun <jesse.luo@rawstone.com.tw>
2023-02-12 19:44:13 +0800

    commit #6 Create an API query message list of the user from MongoDB

--- src/main/java/com/jesse/springlinedemo/line/controller/LineMessageController.java
+++ src/main/java/com/jesse/springlinedemo/line/controller/LineMessageController.java
@@ -1,5 +1,6 @@
 package com.jesse.springlinedemo.line.controller;
 
+import com.jesse.springlinedemo.line.model.LineMessage;
 import com.jesse.springlinedemo.line.service.LineMessageService;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.ReplyMessage;
@@ -14,10 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -69,9 +67,9 @@ public class LineMessageController {
 
     }
 
-//    @GetMapping("/users/{userId}/messages")
-//    public List<LineMessage> getMessage(@PathVariable String userId){
-//        return lineMessageService.getListByUserId(userId);
-//    }
+    @GetMapping("/users/{userId}/messages")
+    public List<LineMessage> getMessage(@PathVariable String userId){
+        return lineMessageService.getListByUserId(userId);
+    }
 
 }
